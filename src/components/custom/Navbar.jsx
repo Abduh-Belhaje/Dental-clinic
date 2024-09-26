4b075cc56c5f88f7a54cdb0c1f96f8157bb8280d
Rida-Lamiini <ridalamiini0@gmail.com>
2024-09-26 23:21:02 +0100

    add shadcn

--- src/components/custom/Navbar.jsx
+++ src/components/custom/Navbar.jsx
@@ -1,17 +1,19 @@
 "use client";
 
-import { useState } from 'react';
-import { Menu, X } from 'lucide-react';
-import Link from 'next/link'; 
+import { useState } from "react";
+import { Menu, X } from "lucide-react";
+import Link from "next/link";
 import Logo from "../../assets/Logo.png";
-import Image from 'next/image';
-import Button from '../ui/Button';
+import Image from "next/image";
+import Button from "../ui/Button";
 
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <nav className="relative "> {/* Set position relative on nav */}
+    <nav className="relative ">
+      {" "}
+      {/* Set position relative on nav */}
       <div className="flex items-center justify-between h-16">
         <div className="flex-shrink-0">
           <Image className="h-8 w-auto" src={Logo} alt="Logo" />
@@ -33,7 +35,10 @@ export default function Navbar() {
           </div>
         </div>
         <div className="hidden md:block">
-          <Button value="Call us" style="border px-5 py-1 rounded-xl bg-white text-primary"/>
+          <Button
+            value="Call us"
+            style="border px-5 py-1 rounded-xl bg-white text-primary"
+          />
         </div>
         <div className="md:hidden">
           <button
@@ -49,9 +54,10 @@ export default function Navbar() {
           </button>
         </div>
       </div>
-
       {isMenuOpen && (
-        <div className="absolute text-white top-15 w-full bg-primary bg-opacity-90 z-50 md:hidden border"> {/* Absolute positioning */}
+        <div className="absolute text-white top-15 w-full bg-primary bg-opacity-90 z-50 md:hidden border">
+          {" "}
+          {/* Absolute positioning */}
           <div className="flex flex-col items-center px-2 pt-2 pb-3 space-y-1 sm:px-3">
             <Link href="/" passHref>
               Home
@@ -68,7 +74,7 @@ export default function Navbar() {
           </div>
           <div className="pt-4 pb-3 border-t border-gray-200">
             <div className="px-2">
-              <Button value="Call us" style="w-full"/>
+              <Button value="Call us" style="w-full" />
             </div>
           </div>
         </div>
