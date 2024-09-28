251f76d97d891229364792980380ac80d33c78ad
Abduh-Belhaje <abduhbelhaj@gmail.com>
2024-09-29 00:40:59 +0100

    modify the header

--- src/components/section/Header.jsx
+++ src/components/section/Header.jsx
@@ -11,31 +11,28 @@ import BackgroundLines from "../../assets/bg.png";
 export default function Header() {
   return (
     <div
-      className="flex flex-col items-center py-10 text-background mb-24 "
-      style={{
-        backgroundImage: `url(${BackgroundLines.src})`,
-        backgroundSize: 'cover', // Change to 'cover' or specify dimensions as needed
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat', // Prevents the image from repeating
-      }}
+      className="flex flex-col items-center text-background mb-36 relative "
       >
-      <div className="py-8 font-playfair flex flex-col items-center md:w-9/12 w-11/12">
-          <div className="py-1 flex items-center md:justify-center justify-evenly w-full">
-            <span className="lg:text-9xl md:text-7xl text-4xl font-medium ">Your dream</span>
-            <Image src={Smile} className="rounded-3xl lg:h-20 lg:w-40 md:h-12 md:w-28 h-8 w-20 md:mx-5" alt=""/>
+        <div className="absolute 0 z-0 flex md:mt-0 mt-20 justify-center items-center">
+            <Image src={BackgroundLines} alt="" className="" />
+        </div>
+      <div className="pb-8 pt-24 z-50 font-playfair flex flex-col items-center md:w-9/12 w-11/12">
+          <div className="flex items-center md:justify-center justify-evenly w-full">
+            <span className="lg:text-9xl md:text-7xl text-4xl font-medium">Your dream</span>
+            <Image src={Smile} className="rounded-full lg:h-20 lg:w-40 md:h-12 md:w-28 h-8 w-20 md:mx-5 object-cover" alt=""/>
             </div>
-          <div className="py-1 flex items-center  md:justify-center justify-evenly w-full">
+          <div className="flex items-center  md:justify-center justify-evenly w-full">
           <span className="lg:text-9xl md:text-7xl text-4xl font-medium">smile</span>
-          <Image src={Smile2} className="rounded-3xl lg:h-20 lg:w-40 md:h-12 md:w-28 h-8 w-20 md:mx-5" alt=""/> 
+          <Image src={Smile2} className="rounded-full lg:h-20 lg:w-40 md:h-12 md:w-28 h-8 w-20 md:mx-5 object-cover " alt=""/> 
             <span className="lg:text-9xl md:text-7xl text-4xl font-medium">is our</span>
             </div>
           <div className="flex md:items-center items-center md:justify-center justify-evenly w-full">
             <Image src={Heart} className="lg:w-28 md:w-16 w-12  md:mr-10" alt=""/>
             <span className="lg:text-9xl md:text-7xl text-4xl font-medium py-1">Care</span> 
-            <button className="border bg-primary lg:text-lg text-xs text-white md:py-4 py-2 lg:px-10 md:px-16 px-8 rounded-3xl sm:mx-10">Book Now !</button> 
+            <button className="border bg-primary lg:text-lg text-xs text-white md:py-4 py-2 lg:px-24 md:px-16 px-8 rounded-3xl sm:mx-10">Book Now !</button> 
             </div>
       </div>
-      <div className="flex md:flex-row flex-col lg:h-40 my-20 w-full ">
+      <div className="flex md:flex-row flex-col lg:h-40 my-20 w-full z-50">
         <div className="flex flex-col px-5 h-full w-fit py-2 min-w-40">
           <span className="text-2xl font-medium">Best Deals</span>
           <span className="text-sm py-2 underline cursor-pointer text-gray-500">
